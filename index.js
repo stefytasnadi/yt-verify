4e910ae046c13bd8c27f0cc7b720cb4ced96d3d8
Stefan Tasnadi <stefytasnadi@yahoo.com>
2020-09-23 14:33:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ client.login(process.env.TOKEN);
 
 client.on("message", message => {
     client.user.setActivity("bot made by Aredray"); 
-    var member; 
-    var image;
 
     if (message.attachments.size > 0) {
         if (message.attachments.every(attachIsImage)){
@@ -18,12 +16,8 @@ client.on("message", message => {
     }     
     function attachIsImage(msgAttach) {
         if (message.channel.name.includes("『🍸』丨sub-proof")){
-            let role = message.guild.roles.cache.find(r => r.name === "Advanced Manelist");
-            var url = msgAttach.url;
-            //True if this url is a png image.
-            image = url.indexOf("png", url.length - "png".length /*or 3*/) !== -1;
-            member = message.author;
-            message.guild.member(member).roles.add(role)
+            var role = message.guild.roles.cache.find(r => r.name === "Advanced Manelist");
+            message.guild.member(message.author).roles.add(role);
             message.react("👍");
             //console.log(message.channel.messages);
             message.author.send('Thank you for subbing, you can now access the rest of the server. Also make sure to read the rules and if you have any question I answer to all dms');
